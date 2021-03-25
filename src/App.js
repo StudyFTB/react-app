2a7dfec5454a06fb8349d683a4c058ebf135bda9
ftb <835571344@qq.com>
2021-03-25 08:41:23 +0800

    feat: 登录页面

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
-import Home from './views/Home';
-import About from './views/About';
+import Login from './views/login/index';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Route component={Home} exact path='/' />
-          <Route component={About} path='/about' />
+          <Route component={Login} exact path='/' />
         </Switch>
       </BrowserRouter>
     )
---
+++ src/views/login/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.scss';
+
+export default class Login extends React.Component {
+  render() {
+    return (
+      <div>login</div>
+    )
+  }
+}
