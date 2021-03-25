ae23699ac94747ab9ae014e21ecbece47c96d4e8
ftb <835571344@qq.com>
2021-03-25 17:42:22 +0800

    x

--- src/views/login/index.js
+++ src/views/login/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './index.scss';
+import { Form, Input } from 'antd';
 
 export default class Login extends React.Component {
   render() {
     return (
-      <div>login</div>
+      <div className="container">
+        <Form>
+          <Form.Item label="Username" name="username" >
+            <Input autoComplete={false} />
+          </Form.Item>
+        </Form>
+      </div>
     )
   }
 }
