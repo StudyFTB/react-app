86ca09b60b3d1d4a17e42e9055a304b9787b4c92
ftb <835571344@qq.com>
2021-10-21 17:59:59 +0800

    fix(route): 根据路由设置标题

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,7 +1,11 @@
 import { Switch, Route, BrowserRouter, Redirect } from 'react-router-dom'
 import routes from '@/router/routes.js'
 
-export default function BaseRouter() {
+export default function BaseRouter(props) {
+  const RouteEnter = (route) => {
+    document.title = route.name || '随便的标题'
+  }
+
   return (
     <BrowserRouter>
       <Switch>
@@ -14,12 +18,12 @@ export default function BaseRouter() {
         {
           routes.map(item => {
             return (
-              <Route exact path={item.path} key={item.path} render={ (props) =>
+              <Route exact path={item.path} key={item.path} onEnter={RouteEnter(item)} render={ (props) =>
                 <item.component {...props}>
                   {
                     item.children?.map(child => {
                       return (
-                        <Route exact component={child.component} path={child.path} key={child.path} />
+                        <Route exact component={child.component} path={child.path} key={child.path} onEnter={RouteEnter(child)} />
                       )
                     })
                   }
