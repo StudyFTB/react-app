71244c2cba0aeea373a1cb99b8fdc1c1e543e6c4
ftb <835571344@qq.com>
2021-10-25 09:16:47 +0800

    fix(redux): 生成redux容器组件

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,11 +1,32 @@
 import React from "react";
+import { addCount, reduceCount } from '@/store/actions.js'
+import { connect } from 'react-redux'
 
 class Home extends React.Component {
   render() {
+    const { value, onReduceClick, onIncreaseClick } = this.props;
+    console.log(value,onIncreaseClick);
     return (
-      <div>首页</div>
+      <div>
+        <button onClick={onReduceClick}>Reduce</button>
+        <span>{value}</span>
+        <button onClick={onIncreaseClick}>Increase</button>
+      </div>
     )
   }
 }
 
-export default Home;
+const mapStateToProps = (state) => {
+  return {
+    value: state.computedCount
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onReduceClick: () => dispatch(reduceCount(1)),
+    onIncreaseClick: () => dispatch(addCount(1))
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Home);
