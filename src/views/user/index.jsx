cbd9049fa1b9647ecc2a5dcd842196c0a4dc95a0
ftb <835571344@qq.com>
2021-11-19 17:14:01 +0800

    x

--- src/views/user/index.jsx
+++ src/views/user/index.jsx
@@ -7,15 +7,37 @@ function User() {
     return () => {
       console.log(2222);
     }
-  })
+  },[count]);
+
+  const [countdown,setCountDown] = useState(30);
+  useEffect(() => {
+    let timer = null;
+    timer = setTimeout(() => {
+      let nCountdown = countdown - 1;
+      if(nCountdown < 0) {
+        return
+      }
+      setCountDown(nCountdown)
+    },1000);
+    return () => {
+      console.log(1111111111)
+      clearTimeout(timer)
+    }
+  },[countdown])
 
   return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
-    </div>
+    <>
+      <div>
+        <p>You clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)}>
+          Click me
+        </button>
+      </div>
+      <div>
+        <button>点击开始倒计时</button>
+        <div>倒计时：{countdown}</div>
+      </div>
+    </>
   )
 }
 
